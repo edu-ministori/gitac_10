41287e447f17c73445bd8d586882c23393523aa0
edu-ministori <88914370+edu-ministori@users.noreply.github.com>
2021-12-11 16:04:26 +0900

    add

--- page.js
+++ page.js
@@ -4,3 +4,48 @@ let gnb = document.querySelector('.gnb');
 hamburger.addEventListener('click', function(){
   gnb.classList.toggle('active');
 });
+
+let carousel = document.querySelectorAll('.carousel');
+const play = document.querySelector('.btn-play');
+const pause = document.querySelector('.btn-pause');
+
+let current = 0;
+let next = 1;
+let prev = 2;
+
+let intervalNumber = 0;
+
+function move(){
+
+  // prev 변수 값이 carousel 개수 범위를 벗어났는지 체크
+  if(prev>carousel.length-1){
+    prev = 0;
+  }
+  // html element에 불필요한 class를 제거
+  for(let i=0; i<carousel.length; i++){
+    carousel[i].classList.remove('current', 'prev', 'next');
+  }
+  // carousel 아이템별로 올바른 class를 추가 => carousel 동작
+  carousel[current].classList.add('prev');
+  carousel[next].classList.add('current');
+  carousel[prev].classList.add('next');
+  // carousel 아이템 번호별로 rolling
+  current = next;
+  next = prev;
+  prev++;
+
+};
+
+intervalNumber = setInterval(function(){
+  move();
+}, 2000);
+
+pause.addEventListener('click', function(){
+  clearInterval(intervalNumber);
+});
+
+play.addEventListener('click', function(){
+  intervalNumber = setInterval(function(){
+    move();
+  }, 2000);
+});
